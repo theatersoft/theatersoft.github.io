b33ed7b0c9944fb78ac9a7a1eb924e3ef087dffd
Rob Huehn <rhuehn@gmail.com>
2017-09-20 10:49:44 -0700

    auto build commit

--- theatersoft.js
+++ theatersoft.js
@@ -3,13 +3,17 @@
 
 var style = { "items": "_nav_items", "icon": "_nav_icon" };
 
-const [items, icon] = [style.items, style.icon].map(document.getElementById.bind(document));
+const id = document.getElementById.bind(document);
+const items = id(style.items);
+const icon = id(style.icon);
+const use = icon.firstChild.firstChild;
 
 icon.addEventListener('click', e => {
     e.preventDefault();
     const expanded = icon.getAttribute('aria-expanded') === 'true';
     icon.setAttribute('aria-expanded', !expanded);
     items.setAttribute('aria-expanded', !expanded);
+    use.setAttribute('href', expanded ? '#svg-list' : '#svg-x');
 }, true);
 
 }());
