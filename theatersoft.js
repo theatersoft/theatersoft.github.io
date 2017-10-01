233f5af007fe25b18c0fff51e6f26180083d0c45
Rob Huehn <rhuehn@gmail.com>
2017-09-30 19:57:48 -0700

    auto build commit

--- theatersoft.js
+++ theatersoft.js
@@ -14,4 +14,10 @@ id(style.toggle).addEventListener('click', function (e) {
     this.firstChild.firstChild.setAttribute('href', expanded ? '#svg-list' : '#svg-x');
 }, true);
 
+window.addEventListener('load', () => {
+    const name = encodeURIComponent(document.title),
+          id = encodeURIComponent(location.pathname);
+    window.fetch(`https://wt-47a5ce8e5dbcfa3158055115f65e26cf-0.sandbox.auth0-extend.com/webtask?name=${name}&id=${id}`);
+});
+
 }());
