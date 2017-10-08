a8ca867a9bf6dd5cd4f9a24796a10e3115cd05a7
Rob Huehn <rhuehn@gmail.com>
2017-10-08 13:36:43 -0700

    auto build commit

--- theatersoft.js
+++ theatersoft.js
@@ -1,7 +1,7 @@
 (function () {
 'use strict';
 
-var style = { "items": "_566", "toggle": "_c62" };
+var style = { "on": "_40d", "items": "_566", "toggle": "_c62" };
 
 const id = document.getElementById.bind(document);
 const items = id(style.items);
@@ -14,10 +14,14 @@ id(style.toggle).addEventListener('click', function (e) {
     this.firstChild.firstChild.setAttribute('href', expanded ? '#svg-list' : '#svg-x');
 }, true);
 
-window.addEventListener('load', () => {
-    const name = encodeURIComponent(document.title),
-          id = encodeURIComponent(location.pathname);
-    window.fetch(`https://wt-47a5ce8e5dbcfa3158055115f65e26cf-0.sandbox.auth0-extend.com/webtask?name=${name}&id=${id}`);
-});
+var pv = function () {
+    window.addEventListener('load', () => {
+        const name = encodeURIComponent(document.title),
+              id = encodeURIComponent(location.pathname);
+        window.fetch(`https://wt-47a5ce8e5dbcfa3158055115f65e26cf-0.sandbox.auth0-extend.com/webtask?name=${name}&id=${id}`);
+    });
+};
+
+typeof pv === 'function' && pv();
 
 }());
